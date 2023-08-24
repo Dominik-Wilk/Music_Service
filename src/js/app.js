309a237f80bbcaa8ab6cf40f4f6549de5ad3564d
Dominik Wilk <mr.wilkd@gmail.com>
2023-08-24 21:15:25 +0100

    Prepare JS files for using in future

---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+import { select, classNames } from './settings.js';
+
+const app = {
+  initPages: function () {
+    this.pages = document.querySelector(select.containerOf.pages).children;
+    this.navLinks = document.querySelectorAll(select.nav.links);
+    console.log(this.navLinks);
+    const idFromHash = window.location.hash.replace('#', '');
+    let pageMatchingHash = this.pages[0].id;
+    for (let page of this.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    this.activatePage(pageMatchingHash);
+
+    for (let link of this.navLinks) {
+      link.addEventListener('click', e => {
+        const clickedElement = e.currentTarget;
+
+        e.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        this.activatePage(id);
+
+        window.location.hash = `#/${id}`;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    for (let page of this.pages) {
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    for (let link of this.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') === `#${pageId}`);
+    }
+  },
+  init: function () {
+    this.initPages();
+  },
+};
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,29 @@
+export const select = {
+  templateOf: {
+    homePage: '#template-home-page',
+  },
+  containerOf: {
+    songs: '#songs',
+    searchResult: '#searchResult',
+    discoverResult: '#discoverResult',
+    home: '.home-wrapper',
+    pages: '#pages',
+  },
+  nav: {
+    links: '.navigation a',
+  },
+  song: {
+    name: '#name',
+    source: '#src',
+    categories: '#categories',
+    rank: '#rank',
+  },
+};
+export const classNames = {
+  nav: {
+    active: 'clicked',
+  },
+  pages: {
+    active: 'active',
+  },
+};
